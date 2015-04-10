faf15889c563f4dcb9afb8d63a3d711021faf52a
Natalie.Miku <natalie.miku@gmail.com>
2015-04-11 01:07:48 +1200

    Updates settings for Compass

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'susy'
+
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'css/sass'
+images_dir = 'images'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
